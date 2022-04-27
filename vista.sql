43884bb470cf55fb3f68a25f7562939af4e8277e
maximka76667 <74974283+maximka76667@users.noreply.github.com>
2022-04-27 13:45:56 +0200

    create view

---
+++ vista.sql
@@ -0,0 +1,7 @@
+create view pedidos as (
+  select distinct orders.orderNumber, orders.orderDate, orders.customerNumber, customers.customerName, sum(priceEach * quantityOrdered) as importeTotal
+  from orders
+  left join customers on orders.customerNumber = customers.customerNumber
+  join orderdetails on orders.orderNumber = orderdetails.orderNumber
+  group by orders.orderNumber
+)
